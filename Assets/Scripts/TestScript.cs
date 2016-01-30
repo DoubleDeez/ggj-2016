58e3759b3f7fa46e2e9ae40f96ae7a3603d83ab5
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2016-01-29 21:13:57 -0800

    Adding basic/test files

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	   Debug.Log("Holy Crap, This is VSCode!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
