faa25b9549d97f908e48a56dd0cf8d40d9dcb4a5
Dylan Dumesnil <dadumesnil@gmail.com>
2016-01-29 23:23:04 -0800

    Added InteractionObject prefab

---
+++ Assets/Scripts/InteractionObjectController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionObjectController : MonoBehaviour {
+    
+    public GameObject InteractionIcon;
+    public GameObject InteractionObjectDefault;
+    public GameObject InteractionIconHighlighted;
+    
+	void Start () {
+	}
+    
+    void OnValidate() {
+        
+    }
+	
+	void Update () {
+	
+	}
+    
+    void OnTriggerEnter(Collider Other) {
+        if(Other.gameObject.tag.Equals("Player")) {
+            InteractionIcon.GetComponent<Renderer>().enabled = true;
+            InteractionObjectDefault.GetComponent<Renderer>().enabled = false;
+            InteractionIconHighlighted.GetComponent<Renderer>().enabled = true;
+        }
+    }
+    
+    void OnTriggerExit(Collider Other) {
+        if(Other.gameObject.tag.Equals("Player")) {
+            InteractionIcon.GetComponent<Renderer>().enabled = false;
+            InteractionObjectDefault.GetComponent<Renderer>().enabled = true;
+            InteractionIconHighlighted.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
