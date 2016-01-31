77d4e7395a78ad8691fe3b6e5a211cb0bee83ee1
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2016-01-31 11:53:51 -0800

    Intro Movie Added

--- Assets/Scripts/UI/MenuManager.cs
+++ Assets/Scripts/UI/MenuManager.cs
@@ -1,20 +1,51 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour {
 
+    public GameObject IntroObject;
+    public GameObject StartButton;
+    public GameObject CreditsButton;
+    public GameObject QuitButton;
+    public float IntroTime = 8.0f;
+    
+    private MovieTexture IntroMovie;
+    private float ElapsedTime=0.0f;
+    
 	// Use this for initialization
-	void Start () {
-	
+	void OnEnable () 
+    {   
+	   IntroMovie = (MovieTexture) IntroObject.GetComponent<RawImage>().texture;
+       IntroMovie.Play();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        //Wait some time for showing the buttons
+        if(!IntroMovie.isPlaying)
+        {
+            OnEnable();
+            ElapsedTime = 0.0f;
+        }
+        else if(ElapsedTime > IntroTime)
+        {
+            StartButton.SetActive(true);
+            CreditsButton.SetActive(true);
+            QuitButton.SetActive(true);
+        }
+        ElapsedTime+=Time.deltaTime;
+        
 	   if(Input.GetButtonDown("Submit"))
        {
            OnPressStart();
        }
 	}
+    
+    private void ResetButtons()
+    {
+        
+    }
     
     public void OnPressStart()
     {
@@ -23,7 +54,7 @@ public class MenuManager : MonoBehaviour {
     
     public void OnPressCredits()
     {
-        // SceneManager.LoadScene
+        SceneManager.LoadScene("CreditsScene");
     }
     
     public void OnPressQuit()
