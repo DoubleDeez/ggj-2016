db7d6a47a80e6741f8d3ece6f1af4d8c253b96da
hfaran <hamzafaran@outlook.com>
2016-01-31 03:53:20 -0800

    Format InputController.cs

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -6,11 +6,12 @@ using System.Collections.Generic;
 /// The InputController has hardcoded strings corresponding to Buttons and Joystick Axis in
 ///  the Unity Input Manager. Changes there have to be reflected here...
 /// </summary>
-public class InputController : MonoBehaviour {
+public class InputController : MonoBehaviour
+{
 
-/// <summary>
-/// Editor Variables
-/// </summary>
+    /// <summary>
+    /// Editor Variables
+    /// </summary>
     public float PlayerVelocity = 6.0f;
     public float PlayerJumpHeight = 6.0f;
     public float PlayerChargeMultiplier = 3.0f;
@@ -19,9 +20,9 @@ public class InputController : MonoBehaviour {
     public AudioClip playerRunningSound;
     public AudioClip playerWalkingSound;
 
-/// <summary>
-/// Private Variables
-/// </summary>
+    /// <summary>
+    /// Private Variables
+    /// </summary>
     private GameStateManager GameState;
 
     private Player MainPlayer;
@@ -30,11 +31,11 @@ public class InputController : MonoBehaviour {
     private Animator PlayerAnimator;
     private float VariableVelocity;
 
-    private int PlayerNumber=1;
-    private bool IsMoving=false;
-    private bool IsInteracting=false;
+    private int PlayerNumber = 1;
+    private bool IsMoving = false;
+    private bool IsInteracting = false;
     // set false or true depending on whether Grandpa or Kid and can jump
-    private bool jumpAllowed=true;
+    private bool jumpAllowed = true;
     private float TranslationMovement;
     private string _currentDirection = "right";
 
@@ -44,61 +45,62 @@ public class InputController : MonoBehaviour {
     private const int GRANDPA_PLAYER_NUM = 2;
 
     // state int to be set to for changing animation
-    private enum AnimStates {
-        Idle=0,
-        Charge=2,
-        Grenade=3,
-        Jump=4,
-        Scared=5,
-        Touch=6,
-        Walk=1,
-        Whisper=7
+    private enum AnimStates
+    {
+        Idle = 0,
+        Charge = 2,
+        Grenade = 3,
+        Jump = 4,
+        Scared = 5,
+        Touch = 6,
+        Walk = 1,
+        Whisper = 7
     }
     private Dictionary<int, string> state_to_name = new Dictionary<int, string>();
 
-    private int currentAnimation = (int) AnimStates.Idle;
+    private int currentAnimation = (int)AnimStates.Idle;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         audioSource = GetComponent<AudioSource>();
 
-       // Define state to name mapping for animations
-       state_to_name.Add(0, "Idle");
-       state_to_name.Add(1, "Walk");
-       state_to_name.Add(2, "Charge");
-       state_to_name.Add(3, "Grenade");
-       state_to_name.Add(4, "Jump");
-       state_to_name.Add(5, "Scared");
-       state_to_name.Add(6, "Touch");
-       state_to_name.Add(7, "Whisper");
+        // Define state to name mapping for animations
+        state_to_name.Add(0, "Idle");
+        state_to_name.Add(1, "Walk");
+        state_to_name.Add(2, "Charge");
+        state_to_name.Add(3, "Grenade");
+        state_to_name.Add(4, "Jump");
+        state_to_name.Add(5, "Scared");
+        state_to_name.Add(6, "Touch");
+        state_to_name.Add(7, "Whisper");
 
-       GameState = FindObjectOfType<GameStateManager>();
+        GameState = FindObjectOfType<GameStateManager>();
 
-	   if(GameState==null)
-       {
-           Debug.Log("GameStateManager missing! Abort!");
-           this.enabled = false;
-           return;
-       }
-       else
-       {
-           MainPlayer = gameObject.GetComponent<Player>();
-           PlayerPhysics = gameObject.GetComponent<Rigidbody2D>();
-           PlayerCollider = gameObject.GetComponent<BoxCollider2D>();
-           PlayerAnimator = gameObject.GetComponent<Animator>();
-           if(MainPlayer!=null && PlayerPhysics!=null)
-           {
-               PlayerNumber = (int) XboxInput;
-               Debug.Log("Setting up for input Player "+PlayerNumber);
-               //No setup anymore!
-           }
-       }
+        if (GameState == null)
+        {
+            Debug.Log("GameStateManager missing! Abort!");
+            this.enabled = false;
+            return;
+        }
+        else
+        {
+            MainPlayer = gameObject.GetComponent<Player>();
+            PlayerPhysics = gameObject.GetComponent<Rigidbody2D>();
+            PlayerCollider = gameObject.GetComponent<BoxCollider2D>();
+            PlayerAnimator = gameObject.GetComponent<Animator>();
+            if (MainPlayer != null && PlayerPhysics != null)
+            {
+                PlayerNumber = (int)XboxInput;
+                Debug.Log("Setting up for input Player " + PlayerNumber);
+                //No setup anymore!
+            }
+        }
 
-	}
+    }
 
-	// Update is called once per frame
-	void Update ()
+    // Update is called once per frame
+    void Update()
     {
         if (!GameState.IsGamePaused())
         {
@@ -111,85 +113,101 @@ public class InputController : MonoBehaviour {
         // {
         //     MainPlayer.SwitchAnimatorController();
         // }
-	}
+    }
 
     // Check and read Input
     private void HandlePlayerInput()
     {
         //Interactions
         IsInteracting = XCI.GetButton(XboxButton.A);
-        if(IsInteracting) {
-            foreach(GameStateManager.LevelInteraction interaction in MainPlayer.GetLevelInteractionsColliding()) {
+        if (IsInteracting)
+        {
+            foreach (GameStateManager.LevelInteraction interaction in MainPlayer.GetLevelInteractionsColliding())
+            {
                 interaction.HasBeenInteracted = true;
                 GameState.DoInteraction(interaction);
             }
         }
 
         float mVelocity = PlayerVelocity;
-        if (isCurrentAnimation("Charge")) {
+        if (isCurrentAnimation("Charge"))
+        {
             mVelocity *= PlayerChargeMultiplier;
         }
         //Jumping
-        if(IsGrounded())
+        if (IsGrounded())
         {
             VariableVelocity = mVelocity;
-            if(XCI.GetButtonDown(XboxButton.X,XboxInput))
+            if (XCI.GetButtonDown(XboxButton.X, XboxInput))
             {
                 Jump();
             }
         }
         else
         {
-            VariableVelocity -= Time.deltaTime*mVelocity/2;
+            VariableVelocity -= Time.deltaTime * mVelocity / 2;
         }
 
-       //Movement (Horizontal only)
-        TranslationMovement =  XCI.GetAxis(XboxAxis.LeftStickX,XboxInput);
-        if (TranslationMovement >= 0.0f) {
+        //Movement (Horizontal only)
+        TranslationMovement = XCI.GetAxis(XboxAxis.LeftStickX, XboxInput);
+        if (TranslationMovement >= 0.0f)
+        {
             changeDirection("right");
-        } else {
+        }
+        else
+        {
             changeDirection("left");
             TranslationMovement *= -1;
         }
-        gameObject.transform.Translate(Time.deltaTime * VariableVelocity * TranslationMovement,0,0);
+        gameObject.transform.Translate(Time.deltaTime * VariableVelocity * TranslationMovement, 0, 0);
 
         // Hint
-        if(XCI.GetButton(XboxButton.B,XboxInput)) {
+        if (XCI.GetButton(XboxButton.B, XboxInput))
+        {
             MainPlayer.ShowHint();
         }
 
         // Charge
-        if(XCI.GetButton(XboxButton.Y,XboxInput)) {
-            if (MainPlayer.PlayerNumber == KID_PLAYER_NUM) {
+        if (XCI.GetButton(XboxButton.Y, XboxInput))
+        {
+            if (MainPlayer.PlayerNumber == KID_PLAYER_NUM)
+            {
                 Charge();
             }
         }
 
         //DPad - Let the player handle this logic
-        if(XCI.GetDPadDown(XboxDPad.Up,XboxInput))
+        if (XCI.GetDPadDown(XboxDPad.Up, XboxInput))
         {
             MainPlayer.OnDPadUp();
         }
-        else if( XCI.GetDPadDown(XboxDPad.Down,XboxInput))
+        else if (XCI.GetDPadDown(XboxDPad.Down, XboxInput))
         {
             MainPlayer.OnDPadDown();
         }
-        else if( XCI.GetDPadDown(XboxDPad.Left,XboxInput))
+        else if (XCI.GetDPadDown(XboxDPad.Left, XboxInput))
         {
             MainPlayer.OnDPadLeft();
         }
-        else if( XCI.GetDPadDown(XboxDPad.Right,XboxInput))
+        else if (XCI.GetDPadDown(XboxDPad.Right, XboxInput))
         {
             MainPlayer.OnDPadRight();
         }
 
-        if(XCI.GetDPadUp(XboxDPad.Up, XboxInput)) {
+        if (XCI.GetDPadUp(XboxDPad.Up, XboxInput))
+        {
             MainPlayer.OnDPadUpReleased();
-        } else if(XCI.GetDPadUp(XboxDPad.Down, XboxInput)) {
+        }
+        else if (XCI.GetDPadUp(XboxDPad.Down, XboxInput))
+        {
             MainPlayer.OnDPadDownReleased();
-        } else if(XCI.GetDPadUp(XboxDPad.Left, XboxInput)) {
+        }
+        else if (XCI.GetDPadUp(XboxDPad.Left, XboxInput))
+        {
             MainPlayer.OnDPadLeftReleased();
-        } else if(XCI.GetDPadUp(XboxDPad.Right, XboxInput)) {
+        }
+        else if (XCI.GetDPadUp(XboxDPad.Right, XboxInput))
+        {
             MainPlayer.OnDPadRightReleased();
         }
     }
@@ -210,33 +228,38 @@ public class InputController : MonoBehaviour {
 
         // Figure out what actual animation is playing right now
         int actualAnimation = -1;
-        foreach (KeyValuePair<int, string> entry in state_to_name) {
-            if (isCurrentAnimation(getAnimationName(entry.Value))) {
+        foreach (KeyValuePair<int, string> entry in state_to_name)
+        {
+            if (isCurrentAnimation(getAnimationName(entry.Value)))
+            {
                 actualAnimation = entry.Key;
             }
         }
 
         // If we want to jump, stop whatever we're doing currently and jump
         if (actualAnimation != currentAnimation &&
-            currentAnimation == (int) AnimStates.Jump) {
-                PlayerAnimator.SetInteger("state", currentAnimation);
-                PlayerAnimator.Play(getAnimationName("Jump"), 0);
-                return;
-            }
-
-        if (actualAnimation == currentAnimation
-            && animationPlayedTimes() >= 0.95f) {
-            currentAnimation = (int) AnimStates.Idle;
+            currentAnimation == (int)AnimStates.Jump)
+        {
+            PlayerAnimator.SetInteger("state", currentAnimation);
+            PlayerAnimator.Play(getAnimationName("Jump"), 0);
+            return;
         }
 
-        if (IsGrounded() && currentAnimation == (int) AnimStates.Idle) {
-            if(Mathf.Abs(TranslationMovement) < 0.1f)
+        if (actualAnimation == currentAnimation
+            && animationPlayedTimes() >= 0.95f)
+        {
+            currentAnimation = (int)AnimStates.Idle;
+        }
+
+        if (IsGrounded() && currentAnimation == (int)AnimStates.Idle)
+        {
+            if (Mathf.Abs(TranslationMovement) < 0.1f)
             {
-                currentAnimation = (int) AnimStates.Idle;
+                currentAnimation = (int)AnimStates.Idle;
             }
             else
             {
-                currentAnimation = (int) AnimStates.Walk;
+                currentAnimation = (int)AnimStates.Walk;
             }
         }
 
@@ -246,80 +269,95 @@ public class InputController : MonoBehaviour {
         PlayerAnimator.SetInteger("state", currentAnimation);
     }
 
-    private void _playSound(AudioClip sound) {
-        if (sound != audioSource.clip || !audioSource.isPlaying) {
+    private void _playSound(AudioClip sound)
+    {
+        if (sound != audioSource.clip || !audioSource.isPlaying)
+        {
             audioSource.clip = sound;
             audioSource.Play();
         }
     }
 
-    private void playSoundForAnim(int anim) {
+    private void playSoundForAnim(int anim)
+    {
         // @TODO Add sounds for rest of animations
-        if (anim == (int) AnimStates.Charge) {
+        if (anim == (int)AnimStates.Charge)
+        {
             _playSound(playerRunningSound);
         }
-        else if (anim == (int) AnimStates.Walk) {
+        else if (anim == (int)AnimStates.Walk)
+        {
             _playSound(playerWalkingSound);
         }
-        else {
+        else
+        {
             Debug.LogWarning(
                 System.String.Format("No such sound for anim {0}", anim)
             );
         }
     }
 
-    public bool Interacted() {
+    public bool Interacted()
+    {
         return IsInteracting;
     }
 
     private void Jump()
     {
-       if (!jumpAllowed) {
-           return;
-       }
+        if (!jumpAllowed)
+        {
+            return;
+        }
 
         PlayerPhysics.AddForce(
-            new Vector2(0, PlayerPhysics.mass * PlayerJumpHeight ),
+            new Vector2(0, PlayerPhysics.mass * PlayerJumpHeight),
             ForceMode2D.Impulse
         );
-        currentAnimation = (int) AnimStates.Jump;
+        currentAnimation = (int)AnimStates.Jump;
     }
 
-    private void Charge() {
+    private void Charge()
+    {
         // if (isCurrentAnimation(getAnimationName("Charge"))) {
         //     return;
         // }
-        currentAnimation = (int) AnimStates.Charge;
+        currentAnimation = (int)AnimStates.Charge;
     }
 
-    private bool isCurrentAnimation(string name) {
+    private bool isCurrentAnimation(string name)
+    {
         return PlayerAnimator.GetCurrentAnimatorStateInfo(0).IsName(name);
     }
 
-    private string getAnimationName(string animationName) {
+    private string getAnimationName(string animationName)
+    {
         string anim_name = "Grandpa" + animationName;
-        if (MainPlayer.PlayerNumber == KID_PLAYER_NUM) {
+        if (MainPlayer.PlayerNumber == KID_PLAYER_NUM)
+        {
             anim_name = "Kid" + animationName;
         }
         return anim_name;
     }
 
-    private void changeDirection(string direction) {
-        if (_currentDirection != direction) {
+    private void changeDirection(string direction)
+    {
+        if (_currentDirection != direction)
+        {
             if (direction == "right")
-             {
-             gameObject.transform.Rotate(0, 180, 0);
-             _currentDirection = "right";
-             }
-             else if (direction == "left")
-             {
-             gameObject.transform.Rotate(0, -180, 0);
-             _currentDirection = "left";
-             }
+            {
+                gameObject.transform.Rotate(0, 180, 0);
+                _currentDirection = "right";
+            }
+            else if (direction == "left")
+            {
+                gameObject.transform.Rotate(0, -180, 0);
+                _currentDirection = "left";
+            }
         }
     }
 
-    private float animationPlayedTimes() {
+    private float animationPlayedTimes()
+    {
         return PlayerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
     }
 
@@ -327,7 +365,7 @@ public class InputController : MonoBehaviour {
     // However, we need hardcoded keyboard input for now...
     private void ReadDebug()
     {
-        if(XboxInput == XboxController.First)
+        if (XboxInput == XboxController.First)
         {
             DebugP1();
         }
@@ -340,29 +378,31 @@ public class InputController : MonoBehaviour {
     private void DebugP1()
     {
         IsInteracting = Input.GetKeyDown(KeyCode.E);
-        if(IsInteracting) {
-            foreach(GameStateManager.LevelInteraction interaction in MainPlayer.GetLevelInteractionsColliding()) {
+        if (IsInteracting)
+        {
+            foreach (GameStateManager.LevelInteraction interaction in MainPlayer.GetLevelInteractionsColliding())
+            {
                 interaction.HasBeenInteracted = true;
                 GameState.DoInteraction(interaction);
             }
         }
 
-        if(IsGrounded())
+        if (IsGrounded())
         {
             VariableVelocity = PlayerVelocity;
 
-            if(Input.GetKeyDown(KeyCode.W))
+            if (Input.GetKeyDown(KeyCode.W))
             {
                 Jump();
             }
         }
         else
         {
-            VariableVelocity -= Time.deltaTime*PlayerVelocity/2;
+            VariableVelocity -= Time.deltaTime * PlayerVelocity / 2;
         }
 
 
-        if(Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             TranslationMovement = -1.0f;
         }
@@ -372,13 +412,14 @@ public class InputController : MonoBehaviour {
         }
         else
         {
-            TranslationMovement = Mathf.Lerp(TranslationMovement,0.0f,Time.deltaTime);
+            TranslationMovement = Mathf.Lerp(TranslationMovement, 0.0f, Time.deltaTime);
         }
 
-        gameObject.transform.Translate(Time.deltaTime * VariableVelocity * TranslationMovement,0,0);
+        gameObject.transform.Translate(Time.deltaTime * VariableVelocity * TranslationMovement, 0, 0);
 
 
-        if(Input.GetKeyDown(KeyCode.Q)) {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             MainPlayer.ShowHint();
         }
     }
@@ -386,28 +427,30 @@ public class InputController : MonoBehaviour {
     private void DebugP2()
     {
         IsInteracting = Input.GetKeyDown(KeyCode.RightShift);
-        if(IsInteracting) {
-            foreach(GameStateManager.LevelInteraction interaction in MainPlayer.GetLevelInteractionsColliding()) {
+        if (IsInteracting)
+        {
+            foreach (GameStateManager.LevelInteraction interaction in MainPlayer.GetLevelInteractionsColliding())
+            {
                 interaction.HasBeenInteracted = true;
                 GameState.DoInteraction(interaction);
             }
         }
 
-        if(IsGrounded())
+        if (IsGrounded())
         {
             VariableVelocity = PlayerVelocity;
 
-            if(Input.GetKeyDown(KeyCode.UpArrow))
+            if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 Jump();
             }
         }
         else
         {
-            VariableVelocity -= Time.deltaTime*PlayerVelocity/2;
+            VariableVelocity -= Time.deltaTime * PlayerVelocity / 2;
         }
 
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             TranslationMovement = -1.0f;
         }
@@ -417,12 +460,13 @@ public class InputController : MonoBehaviour {
         }
         else
         {
-            TranslationMovement = Mathf.Lerp(TranslationMovement,0.0f,Time.deltaTime);
+            TranslationMovement = Mathf.Lerp(TranslationMovement, 0.0f, Time.deltaTime);
         }
 
-        gameObject.transform.Translate(Time.deltaTime * VariableVelocity * TranslationMovement,0,0);
+        gameObject.transform.Translate(Time.deltaTime * VariableVelocity * TranslationMovement, 0, 0);
 
-        if(Input.GetKeyDown(KeyCode.Return)) {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
             MainPlayer.ShowHint();
         }
     }
