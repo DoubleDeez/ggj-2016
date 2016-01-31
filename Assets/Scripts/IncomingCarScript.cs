195a059fba719c76f81f426ecda3dc6fa46f602c
Dylan Dumesnil <dadumesnil@gmail.com>
2016-01-31 01:10:24 -0800

    vroom vroom and objects

---
+++ Assets/Scripts/IncomingCarScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncomingCarScript : MonoBehaviour {
+    
+    public Vector3 StartScale = new Vector3(1,1,1);
+    public Vector3 MaxScale = new Vector3(3,3,3);
+    public float ScaleSpeed = 2.0f;
+    
+    public bool RunHimDown = false;
+
+	// Use this for initialization
+	void Start () {
+	   transform.localScale = StartScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if(RunHimDown) {
+           transform.localScale = Vector3.Lerp(transform.localScale, MaxScale, Time.deltaTime * ScaleSpeed);
+       }
+	}
+}
