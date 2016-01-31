196636ddc53d3e8690637f1b65dcbe7ffcfff65a
Javier E. Fajardo <j_fajard@encs.concordia.ca>
2016-01-31 09:36:49 -0800

    Began Level Scripting Use by calling DoScriptedAction

---
+++ Assets/Scripts/Levels/KidHouse.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KidHouse : MonoBehaviour {
+
+    public GameObject Level;
+    public Sprite OpenDoor;
+
+    private SpriteRenderer LevelBackground;
+    
+	// Use this for initialization
+	void Start () 
+    {
+	   LevelBackground = Level.GetComponent<SpriteRenderer>();
+       if(LevelBackground==null)
+       {
+           Debug.Log("Error loading "+this.name);
+           this.enabled = false;
+       }
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	   
+	}
+    
+    public void DoScriptedAction()
+    {
+        
+    }
+}
